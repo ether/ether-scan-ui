aeb5ff226b4f06326a88a29efc24be6ac05ae36d
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-01-21 20:14:14 +0100

    Revert back to browser router.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createHashRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
 import {RouterProvider} from "react-router";
 import {Statistics} from "@/pages/statistics.tsx";
 import axios from "axios";
 
-const router = createHashRouter(createRoutesFromElements(
-    <Route path="/" element={<App/>}>
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path={import.meta.env.BASE_URL} element={<App/>}>
         <Route index  element={<Navigate to="/statistics"/>}/>
         <Route  path="/statistics" element={<Statistics/>}/>
     </Route>
