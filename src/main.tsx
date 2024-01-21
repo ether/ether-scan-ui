7c97b0635162e6c10463c5c51248c98f3b75dae3
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-01-21 20:04:36 +0100

    Use hashrouter as browser router is not well supported.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
+import {createHashRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
 import {RouterProvider} from "react-router";
 import {Statistics} from "@/pages/statistics.tsx";
 import axios from "axios";
 
-const router = createBrowserRouter(createRoutesFromElements(
+const router = createHashRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
         <Route index  element={<Navigate to="/statistics"/>}/>
         <Route  path="/statistics" element={<Statistics/>}/>
