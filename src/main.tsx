ce6114e9a6bfbcb9fbed3848ad85c9769358541d
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-01-21 20:43:00 +0100

    Use hash router for reloading.

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {createBrowserRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
+import {createHashRouter, createRoutesFromElements, Navigate, Route} from "react-router-dom";
 import {RouterProvider} from "react-router";
 import {Statistics} from "@/pages/statistics.tsx";
 import axios from "axios";
 
-const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<App/>}>
+const router = createHashRouter(createRoutesFromElements(
+    <Route element={<App/>}>
         <Route index  element={<Navigate to="/statistics"/>}/>
         <Route  path="/statistics" element={<Statistics/>}/>
     </Route>
-),{
-    basename: import.meta.env.BASE_URL,
-})
+))
 
 
 
