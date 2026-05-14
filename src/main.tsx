c3a70da2d5200d964946e7ccfe34b8d0cb74b0e3
Stefan Müller <stefan@stefans-entwicklerecke.de>
2026-05-14 22:19:24 +0200

    Add new instance detail page

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import {createBrowserRouter, createRoutesFromElements, Route} from "react-router
 import {RouterProvider} from "react-router";
 import {Statistics} from "@/pages/statistics.tsx";
 import {Instances} from "@/pages/instances.tsx";
+import {InstanceDetail} from "@/pages/instance.tsx";
 import {Toaster} from "@/components/ui/toaster.tsx";
 import {Home} from "@/pages/home.tsx";
 
@@ -14,6 +15,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route index element={<Home/>}/>
         <Route  path="/statistics" element={<Statistics/>}/>
         <Route path={"/instances"} element={<Instances/>}/>
+        <Route path={"/instances/:name"} element={<InstanceDetail/>}/>
     </Route>
 ), { basename: import.meta.env.BASE_URL })
 
