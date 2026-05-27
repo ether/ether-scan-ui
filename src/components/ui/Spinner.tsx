f15a52b1a8ad68230a88a7b10d63fea36fba2a8f
Stefan Müller <stefan@stefans-entwicklerecke.de>
2026-05-28 01:15:23 +0200

    Use loading svg of etherpad

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,6 +1,6 @@
 import {SVGProps} from "react";
-import {cn} from "@/lib/utils.ts";
 import {createPortal} from "react-dom";
+import {Card} from "@/components/ui/card.tsx";
 
 export interface ISVGProps extends SVGProps<SVGSVGElement> {
     size?: number;
@@ -8,29 +8,18 @@ export interface ISVGProps extends SVGProps<SVGSVGElement> {
 }
 
 export const LoadingSpinner = ({
-                                   size = 24,
+                                   size = 128,
                                    className,
                                    ...props
                                }: ISVGProps) => {
     return createPortal(
-        <div className="flex bg-gray-900 pl-5 pr-5 pt-2 pb-2 rounded" id="loading-inner">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={size}
-                height={size}
-                {...props}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className={cn("animate-spin", className)}
-            >
-                <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
-            </svg>
-            <span className="ml-2 loading">Loading<span>.</span><span>.</span><span>.</span></span>
-
+        <div className="flex pl-5 pr-5 pt-2 pb-2 rounded" id="loading-inner">
+            <Card className="border-border bg-accent">
+                <img width={size} height={size} src='/brand.svg' {...props} alt='Loading animation'/>
+                <div style={{textAlign: 'center'}}>
+                    <span  className="ml-2 loading">Loading<span>.</span><span>.</span><span>.</span></span>
+                </div>
+            </Card>
         </div>, document.getElementById('loading')!)
 
 };
