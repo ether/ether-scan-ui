cdbe6a6cb1e2da8b460e02651bda2fca322cd233
Stefan Müller <stefan@stefans-entwicklerecke.de>
2026-06-10 19:59:33 +0200

    fix: remove also className as parameter

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -8,7 +8,6 @@ export interface ISpinnerProps extends ImgHTMLAttributes<HTMLImageElement> {
 
 export const LoadingSpinner = ({
                                    size = 128,
-                                   className,
                                    ...props
                                }: ISpinnerProps) => {
     return createPortal(
